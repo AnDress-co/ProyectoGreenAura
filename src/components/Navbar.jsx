d942b48cc386f51c5a5036883c08a93ee18503a7
AnDress-co <gomezmarlon872@gmail.com>
2026-04-02 19:02:20 -0500

    Create dashboard and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,24 @@
-//Navbar del Dashboard
+const Navbar = () => {
+  return (    
+    <nav className="navbar navbar-expand-lg navbar-custom">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">GreenAura</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" aria-current="page" href="#">Inicio</a>
+            <a className="nav-link" href="#">Nuestra Filosofía</a>
+            <a className="nav-link" href="#">Novedades</a>
+          </div>
+        </div>
+        <div>
+          <button type="button" className="btn btn-success">Prueba</button>
+        </div>
+      </div>
+    </nav>     
+  )
+}
+
+export default Navbar
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,1 +1,14 @@
-//Codigo del Dashboard principal
+import Navbar from "../components/Navbar"
+
+const Dashboard = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container text-center">
+        <h1>GreenAura</h1>
+      </div>      
+    </>
+  )
+}
+
+export default Dashboard
