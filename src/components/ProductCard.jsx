fdef8438b5dbd9c5216a16576ccd4d8e9a4e1d06
juan deyan <juan.chaverra80@correo.tdea.edu.co>
2026-04-23 18:18:26 -0500

    ProductCards Clickeables

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,33 +1,37 @@
-const ProductCard = ({ title, description, discount, image, width }) => {
+import { Link } from "react-router-dom"
+//Lo que recibe los productos (Dependiendo de los que crees)
+const ProductCard = ({ title, description, discount, image, width, link }) => {
   return (
-    <div
-      className="card m-3 p-3 shadow position-relative navbar-custom"
-      style={{ width: width }}
-    >
-      {/* Badge descuento */}
-      <span className="badge bg-success position-absolute top-0 end-0 m-2">
-        {discount}
-      </span>
+    <Link to={link} className="product-link">
+      <div
+        className="card product-card m-3 p-3 shadow position-relative navbar-custom"
+        style={{ width: width }}
+      >
+        {/* Badge descuento */}
+        <span className="badge bg-success position-absolute top-0 end-0 m-2">
+          {discount}
+        </span>
 
-      <div className="d-flex align-items-center">
-        <div className="p-3 flex-grow-1">
-          <h5>{title}</h5>
-          <p>{description}</p>
+        <div className="d-flex align-items-center">
+          <div className="p-3 flex-grow-1">
+            <h5>{title}</h5>
+            <p>{description}</p>
+          </div>
+
+          <img
+            src={image}
+            alt={title}
+            style={{
+              width: "260px",
+              height: "280px",
+              objectFit: "cover",
+              borderRadius: "10px",
+              marginRight: "10px",
+            }}
+          />
         </div>
-
-        <img
-          src={image}
-          alt={title}
-          style={{
-            width: "260px",
-            height: "280px",
-            objectFit: "cover",
-            borderRadius: "10px",
-            marginRight: "10px",
-          }}
-        />
       </div>
-    </div>
+    </Link>
   );
 };
 
