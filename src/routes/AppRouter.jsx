0069554f44c3d8992ce80d00afd0d3a43ce97c1a
AnDress-co <gomezmarlon872@gmail.com>
2026-04-23 19:27:35 -0500

    repair erros

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -11,27 +11,28 @@ import WhatsNew from '../pages/WhatsNew'
 import Dashboard from '../pages/Dashboard'
 
 
-const AppRouter = () => {
-  const location = useLocation()
+const AppRouter = () => {  
 
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/our-philosophy" element={<OurPhilosophy />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/promotions' element={<WhatsNew />}/>
-        <Route path='/dashboard' element={<ProtectedRoute><Dashboard /></ProtectedRoute>}/>
-        {/* Ruta para manejar error HTTP 404 */}
-        <Route path="*" element=
-          {<ErrorPage 
-            code="404" 
-            title="la pagina no se ha encontrado"
-            description="Debes de haber escogido la puerta incorrecta, ya que no he podido encontrar la página que buscas."
-          />}
-        />      
-      </Routes>
-    </AuthProvider>
+    <AnimatePresence>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/our-philosophy" element={<OurPhilosophy />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/promotions' element={<WhatsNew />}/>
+          <Route path='/dashboard' element={<ProtectedRoute><Dashboard /></ProtectedRoute>}/>
+          {/* Ruta para manejar error HTTP 404 */}
+          <Route path="*" element=
+            {<ErrorPage 
+              code="404" 
+              title="la pagina no se ha encontrado"
+              description="Debes de haber escogido la puerta incorrecta, ya que no he podido encontrar la página que buscas."
+            />}
+          />      
+        </Routes>
+      </AuthProvider>
+    </AnimatePresence>
   )
 }
 
